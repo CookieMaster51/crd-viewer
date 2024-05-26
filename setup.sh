15e17303ebdda6966e45554c0cd7a292fa4f8588
Jan Matis <76244176+CookieMaster51@users.noreply.github.com>
2024-05-26 16:18:54 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+   14  python3 -m venv crd-viewer
+
+   16  source crd-viewer/bin/activate
+
+   30  ../bin/pip3 install flask
+   32  ../bin/pip3 install requests
+   44  ../bin/pip3 install pyyaml
+
+git checkout
+
+
+   38  ../bin/flask run --host=0.0.0.0
